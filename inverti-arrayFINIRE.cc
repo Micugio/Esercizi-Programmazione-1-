7dc93adeba19932aca762a980928579e30558c09
Giovannini GiovanniLuca (ACC0207278) <giovanni.giovannini@sci-b106pc043x.unitn.it>
2025-11-06 12:17:43 +0100

    Laboratorio 06/11/2025

--- inverti-arrayFINIRE.cc
+++ inverti-arrayFINIRE.cc
@@ -12,14 +12,14 @@ Non vedo invertire tutti i valori, devo fermarmi a metà, se indice è dispari (
 */
 
 void inizializza (int array[], int dim) {
-    for (int i=  0; i < dim; i++) {
+    for (int i=0; i<dim; i++) {
         array[i] = rand() % 10;
     }
     return;
 }
 
 void stampa (const int array[], int dim) {
-    for (int i = 0; i < dim; i++) {
+    for (int i=0; i<dim; i++) {
         cout << array[i] << " ";
     }
     cout << endl;
@@ -28,7 +28,7 @@ void stampa (const int array[], int dim) {
 
 void inverti (int array[], int dim) {
     int flag = 0;
-    for (int dim; dim > (dim/2); dim--) {
+    for (int dim; dim>(dim/2); dim--) {
         flag = dim;
         cout << array[dim] << " ";
     }
